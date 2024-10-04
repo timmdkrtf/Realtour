a08309fa813da1836016ad781cb36a45addd91df
MHamdanHariri20 <m.hamdanhariri5@gmail.com>
2024-10-04 16:44:12 +0700

    revisi text pt and revisi color green

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ function About(){
                         <div className="title">
                             <h2>Tentang <span>Kami</span></h2>
                         </div>
-                        <p>Realtour adalah persembahan dari PT Abdi Sarana Wisata yang merupakan perusahaan manajemen perjalanan korporat yang berkomitmen untuk menyediakan solusi lengkap bagi kebutuhan perjalanan Anda. Kami bekerja sama dengan Anda untuk menentukan destinasi yang ideal, memilih penginapan yang nyaman, dan merencanakan kuliner serta kegiatan yang mendukung keberlanjutan lingkungan.</p>
+                        <p>Realtour adalah persembahan dari PT Abdi Solusi Wisata yang merupakan perusahaan manajemen perjalanan korporat yang berkomitmen untuk menyediakan solusi lengkap bagi kebutuhan perjalanan Anda. Kami bekerja sama dengan Anda untuk menentukan destinasi yang ideal, memilih penginapan yang nyaman, dan merencanakan kuliner serta kegiatan yang mendukung keberlanjutan lingkungan.</p>
                         <p>Selain itu, kami juga menyusun aktivitas menyenangkan lainnya yang sesuai dengan preferensi Anda. Dengan pendekatan kami yang terintegrasi, setiap perjalanan Anda akan dirancang untuk memberikan pengalaman yang nyaman, menyenangkan, dan penuh makna, memastikan bahwa setiap momen perjalanan Anda menjadi pengalaman yang tak terlupakan.</p>
                     </div>
                 </div>
