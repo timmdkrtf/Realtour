5113a7bcfa1c00f19040a06ff1cf28c088bc298a
timmdkrtf <contact@mdkrtf.co.id>
2025-07-14 04:56:25 -0500

    revisi testimoni realtour

--- src/components/Testimoni.jsx
+++ src/components/Testimoni.jsx
@@ -1,23 +1,66 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+export default function Testimoni() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-export default function Testimoni(){
-    return(
-        <div className="testimoni">
-            <div className="container">
-                <div className="content">
-                    <div className="title">
-                        <h4>Apa kata Mereka?</h4>
-                    </div>
-                    <div className="sticky">
-                        <div className="note a">
-                        </div>
-                        <div className="note b">
-                        </div>
-                        <div className="note c">
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const testimonials = [
+    // {
+    //   id: 1,
+    //   class: `note a`,
+    // },
+    {
+      id: 2,
+      class: `note b`,
+    },
+    {
+      id: 3,
+      class: `note c`,
+    },
+  ];
+
+  return (
+    <div className="testimoni">
+      <div className="container">
+        <div className="content">
+          <div className="title">
+            <h4>Apa kata Mereka?</h4>
+          </div>
+          <div className="sticky">
+            <Slider {...settings}>
+              {testimonials.map((item) => (
+                <div key={item.id} className={item.class}></div>
+              ))}
+            </Slider>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
